e307bba01bd9feac8f7a4626a0899cd36b901fda
Adrian Balan <31134745+intelpen@users.noreply.github.com>
2025-01-30 23:38:48 +0200

    Update dialogs.py

--- frontend/dialogs.py
+++ frontend/dialogs.py
@@ -248,9 +248,9 @@ def generate_pdf(responses):
 
 # Display the generated PDF in the app
 def display_pdf(pdf_path):
-    with open(pdf_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    with open(pdf_path, "rb") as pdf_file:
+        base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
